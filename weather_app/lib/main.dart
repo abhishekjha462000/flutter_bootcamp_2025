45bf61d322fde531f482c9354e7225329b25cafc
Abhishek jha (دانيال) <sameerjha462000@gmail.com>
2025-02-22 23:29:28 +0530

    .

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:weatherapp/weather_app.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: const WeatherApp(),
+    );
+  }
+}
